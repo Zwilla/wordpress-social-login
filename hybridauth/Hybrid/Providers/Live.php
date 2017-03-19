c2606a288608b4c8ec78cbfaea2e30bbd3ed62ed
Zwilla <info@zwilla.de>
2017-03-19 03:16:16 +0100

    Update Live.php

--- hybridauth/Hybrid/Providers/Live.php
+++ hybridauth/Hybrid/Providers/Live.php
@@ -2,7 +2,8 @@
 /*!
 * HybridAuth
 * http://hybridauth.sourceforge.net | http://github.com/hybridauth/hybridauth
-* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html 
+* (c) 2009-2012, HybridAuth authors | http://hybridauth.sourceforge.net/licenses.html
+* Add to by: https://the-internet-of-money.de
 */
 
 /** 
